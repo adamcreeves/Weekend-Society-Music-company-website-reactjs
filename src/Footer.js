2fea894199a911302dda39d595a57f103c551732
Adam Reeves <admchristopher@gmail.com>
2021-06-05 17:10:09 -0700

    Small adjustment with Client

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='footer__copyright'>© 2021 The Weekend Society</div>
-            <button className='footer__button'><a href="mailto:info@weekendsocietymusic.com" className='footer__buttonText'>BOOK NOW</a></button> 
-            <div className='footer__email'>info@weekendsocietymusic.com</div>           
+            <button className='footer__button'><a href="mailto:hello@weekendsocietymusic.com" className='footer__buttonText'>BOOK NOW!</a></button> 
+            <div className='footer__email'>hello@weekendsocietymusic.com</div>           
         </div>
     );
 };
