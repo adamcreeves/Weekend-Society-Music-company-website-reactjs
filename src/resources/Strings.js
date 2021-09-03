32554772be9c4ba84cfab7c1fa32c378bd96c2e3
Adam Reeves <admchristopher@gmail.com>
2021-09-03 12:31:49 -0700

    String literals for long strings

--- src/resources/Strings.js
+++ src/resources/Strings.js
@@ -15,10 +15,31 @@ export const Email = 'hello@weekendsocietymusic.com';
 export const BookNow = 'BOOK NOW!';
 
 // About Screen
-export const BandBio1 = 'The Weekend Society is a high energy dance band committed to making the best day of your life, the actual best day of your life, and will effortlessly take your event from dinner all the way through to the dance party. Based in Los Angeles, we offer not only a wealth of talent but also  a wide range of genres and band sizes to custom fit your special occasion. Our members bring their passion, joy, and love for live music, as well as years of experience sharing the stage with artists such as Gwen Stefani, Kanye West, John Legend, Jennifer Hudson, and Andy Grammer.';
-export const BandBio2 = 'To complement your reception, we bring along event lighting and a premium sound system in addition to MC services. A cocktail hour trio, quartet, or quintet can join your celebration and provide an ambient, jazzy background as you sip your favorite spirits. We also offer ceremony musicians and love to travel. Book The Weekend today!';
+export const BandBio1 = `The Weekend Society is a high energy dance band 
+                        committed to making the best day of your life, 
+                        the actual best day of your life, and will effortlessly 
+                        take your event from dinner all the way through 
+                        to the dance party. Based in Los Angeles, we offer 
+                        not only a wealth of talent but also  a wide range 
+                        of genres and band sizes to custom fit your special 
+                        occasion. Our members bring their passion, joy, and 
+                        love for live music, as well as years of experience 
+                        sharing the stage with artists such as Gwen Stefani, 
+                        Kanye West, John Legend, Jennifer Hudson, and Andy Grammer.`;
+export const BandBio2 = `To complement your reception, we bring along event 
+                        lighting and a premium sound system in addition to MC 
+                        services. A cocktail hour trio, quartet, or quintet 
+                        can join your celebration and provide an ambient, 
+                        jazzy background as you sip your favorite spirits. 
+                        We also offer ceremony musicians and love to travel. 
+                        Book The Weekend today!`;
 export const IsaacBioTitle = 'Isaac Johnson, Owner';
-export const IsaacBio = 'Born and raised in Los Angeles with a Bachelors in Music Performance, Isaac has performed at countless weddings, private events, corporate and special celebrations. Isaac is an avid lover of cinema, enjoys making craft cocktails, and is absolutely in love with his 3 year old daughter.';
+export const IsaacBio = `Born and raised in Los Angeles with a Bachelors in 
+                        Music Performance, Isaac has performed at countless 
+                        weddings, private events, corporate and special 
+                        celebrations. Isaac is an avid lover of cinema, enjoys 
+                        making craft cocktails, and is absolutely in love with 
+                        his 3 year old daughter.`;
 
 // Contact Screen
 export const ContactHeadline = 'Contact us at:';
