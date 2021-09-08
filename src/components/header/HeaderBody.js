0cc19e5637c094666816c4b9abcc7e188e5d5ef6
Adam Reeves <admchristopher@gmail.com>
2021-09-07 20:34:46 -0700

    Clean up
    2 hard coded integers extracted

--- src/components/header/HeaderBody.js
+++ src/components/header/HeaderBody.js
@@ -7,17 +7,18 @@ import { mobileMenuOff, mobileMenuOn } from './utils/HeaderUtils';
 import { c_0022, c_0070, c_0071, c_0072 } from '../../resources/ClassNames';
 import { id_01 } from '../../resources/Strings';
 import "../../styling/components/header/HeaderBody.css";
+import { int_03, int_04 } from '../../resources/Integers';
 
 function HeaderBody() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(int_03);
 
     const toggleMenu = () => {
-        if(count === 0) {
+        if(count === int_03) {
             document.getElementById(id_01).style = mobileMenuOn;
-            setCount(1);
+            setCount(int_04);
         } else {
             document.getElementById(id_01).style = mobileMenuOff;
-            setCount(0);
+            setCount(int_03);
         }
     }
 
